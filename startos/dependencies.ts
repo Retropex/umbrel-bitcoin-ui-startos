f12826a8f9c2a1db1200f0402a67b2d4abc947c8
Léo Haf <leo@haf.ovh>
2025-11-28 13:23:37 +0000

    some fixes

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -1,5 +1,23 @@
 import { sdk } from './sdk'
+import { other } from 'bitcoin-knots/startos/actions/config/other'
 
-export const setDependencies = sdk.setupDependencies(
-  async ({ effects }) => ({}),
-)
+export const setDependencies = sdk.setupDependencies(async ({ effects }) => {
+  await sdk.action.createTask(effects, 'bitcoind', other, 'critical', {
+    input: {
+      kind: 'partial',
+      value: {
+        zmqEnabled: true,
+      },
+    },
+    when: { condition: 'input-not-matches', once: false },
+    reason: 'Umbrel UI require ZMQ',
+  })
+
+  return {
+    bitcoind: {
+      kind: 'running',
+      versionRange: '>=29.1:2-beta.0',
+      healthChecks: ['primary'],
+    },
+  }
+})
--- startos/main.ts
+++ startos/main.ts
@@ -1,5 +1,6 @@
 import { sdk } from './sdk'
 import { uiPort } from './utils'
+import { manifest } from 'bitcoin-knots/startos/manifest'
 
 export const main = sdk.setupMain(async ({ effects, started }) => {
   console.info('Starting Umbrel UI.')
@@ -11,12 +12,29 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
       sdk.Mounts.of().mountVolume({
         volumeId: 'main',
         subpath: null,
-        mountpoint: '/mnt',
+        mountpoint: '/root',
         readonly: false,
-      }),
+      }).mountDependency<typeof manifest>({
+            dependencyId: 'bitcoind',
+            volumeId: 'main',
+            subpath: null,
+            mountpoint: '/mnt/knots',
+            readonly: true,
+          }),
       'umbrel-bitcoin-ui-sub',
     ),
-    exec: { command: ['node /app/dist/server.js'] },
+    exec: { 
+      command: [
+          'env',
+          'BITCOIND_EXTERNAL_MODE=true',
+          'ZMQ_HASHTX_PORT=28333',
+          'ZMQ_HASHBLOCK_PORT=28332',
+          'BITCOIND_IP=bitcoind.startos',
+          'RPC_COOKIE=/mnt/knots/.cookie',
+          'node',
+          '/app/dist/server.js',
+        ] 
+      },
     ready: {
       display: 'Web Interface',
       fn: () =>
