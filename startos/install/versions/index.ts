6274d7965e023ff6b4897786f50452c77ffe831c
Léo Haf <leo@haf.ovh>
2026-04-01 14:23:04 +0000

    Update to SDK 1.0.0

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,3 +1,3 @@
-export { v_1_1_0_6 as current } from './v1.1.0.6'
+export { v_1_1_0_7 as current } from './v1.1.0.7'
 
 export const other = []
--- startos/install/versions/v1.1.0.7.ts
+++ startos/install/versions/v1.1.0.7.ts
@@ -1,8 +1,8 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 
-export const v_1_1_0_6 = VersionInfo.of({
-  version: '1.1.0:6',
-  releaseNotes: 'Update to SDK 65',
+export const v_1_1_0_7 = VersionInfo.of({
+  version: '1.1.0:7',
+  releaseNotes: 'Update to SDK 1.0.0',
   migrations: {
     up: async ({ effects }) => {},
     down: async ({ effects }) => {},
